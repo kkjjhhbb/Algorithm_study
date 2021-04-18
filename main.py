2b43460a25d390f490b73d3a7dfd654da9fb85b0
devjihyun <>
2021-04-18 12:18:31 +0000

    인구이동 마무리

--- main.py
+++ main.py
@@ -1,99 +1,71 @@
 from collections import deque
-n,l,r = map(int,input().split())
-arr=[]
-gap_count = False
-for i in range(n):
-  arr.append(list(map(int,input().split())))
-chec=[[0]*n for _ in range(n)]
-g_sum =1
-answer=0
 
-dx=[1,-1,0,0]
-dy=[0,0,1,-1]
+# 땅의 크기(N), L, R 값을 입력받기
+n, l, r = map(int, input().split())
+
+# 전체 나라의 정보(N x N)를 입력 받기
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
+
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 
 def prin():
   print()
   for i in range(n):
     for j in range(n):
-      print(chec[i][j],end=' ')
+      print(union[i][j],end=' ')
     print()
+  print()
+# 특정 위치에서 출발하여 모든 연합을 체크한 뒤에 데이터 갱신
+def process(x, y, index):
+    # (x, y)의 위치와 연결된 나라(연합) 정보를 담는 리스트
+    united = []
+    united.append((x, y))
+    # 너비 우선 탐색 (BFS)을 위한 큐 라이브러리 사용
+    q = deque()
+    q.append((x, y))
+    union[x][y] = index # 현재 연합의 번호 할당
+    summary = graph[x][y] # 현재 연합의 전체 인구 수
+    count = 1 # 현재 연합의 국가 수
+    # 큐가 빌 때까지 반복(BFS)
+    while q:
+        x, y = q.popleft()
+        # 현재 위치에서 4가지 방향을 확인하며
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 바로 옆에 있는 나라를 확인하여
+            if 0 <= nx < n and 0 <= ny < n and union[nx][ny] == -1:
+                # 옆에 있는 나라와 인구 차이가 L명 이상, R명 이하라면
+                if l <= abs(graph[nx][ny] - graph[x][y]) <= r:
+                    q.append((nx, ny))
+                    # 연합에 추가하기
+                    union[nx][ny] = index
+                    summary += graph[nx][ny]
+                    count += 1
+                    united.append((nx, ny))
+    # 연합 국가끼리 인구를 분배
+    for i, j in united:
+        graph[i][j] = summary // count
 
-def union_check(x,y,inx):
-  gap_sum=0
-  queue=deque()
-  queue.append((x,y))
+total_count = 0
 
-  while queue:
-    x,y=queue.popleft()
-    for i in range(4):
-      nx = x +dx[i]
-      ny = y +dy[i]
-      if nx>=0 and ny>=0 and nx<n and ny<n:
-        gap = abs(arr[x][y] - arr[nx][ny])
-        if chec[nx][ny] == False and l <= gap <= r:
-          chec[nx][ny] = inx
-          queue.append((nx,ny))
-  prin()
-  return gap_sum
-
-def do_union():
-  index=[]
-  sum=0
-  for i in range(n):
-    for j in range(n):
-      if chec[i][j] == True:
-        index.append((i,j))
-        sum += arr[i][j]
-  if sum != 0:
-    sum = sum // len(index)
-    for i in range(len(index)):
-      x,y = index.pop()
-      arr[x][y] = sum
-  prin()
-  return
-
-def end_check():
-  checking=deque()
-  for i in range(n):
-    for j in range(n):
-      if chec[i][j] == False:
-        checking.append((i,j))
-        print(i,j)
-  while checking:
-    x,y=checking.popleft()
-    for i in range(4):
-      nx=x+dx[i]
-      ny=y+dy[i]
-
-      if nx>=0 and ny>=0 and nx<n and ny<n:
-        gap = abs(arr[x][y] - arr[nx][ny])
-        print('gap',gap)
-        if gap<l or gap>r:
-          continue
-        else:
-          return True
-  return False
-
-
-
-
-def do():
-  global answer
-  global g_sum
-  inx=1
-  while True:
+# 더 이상 인구 이동을 할 수 없을 때까지 반복
+while True:
+    union = [[-1] * n for _ in range(n)]
+    index = 0
     for i in range(n):
-      for j in range(n):
-        union_check(i,j,inx)
-        inx += 1
-    do_union()
-    if end_check() == False:
-      answer += 1
-      return answer
-    chec=[[False]*n for _ in range(n)]
-    answer += 1
+        for j in range(n):
+            if union[i][j] == -1: # 해당 나라가 아직 처리되지 않았다면
+                process(i, j, index)
+                prin()
+                index += 1
+    # 모든 인구 이동이 끝난 경우
+    if index == n * n:
+        break
+    total_count += 1
 
-  
-      
-
-print(do())
+# 인구 이동 횟수 출력
+print(total_count)
