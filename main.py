99228b9bab3ef725fb63f022fbc512455a31167c
지현 <37644129+jihyun12@users.noreply.github.com>
2021-07-01 08:57:57 +0000

    문자열 압축 -zip함수 활용

--- main.py
+++ main.py
@@ -1,50 +1,28 @@
-n,m=map(int,input().split())
-x,y,direction=map(int,input().split())
-#[0,1,2,3] [북,동,남,서]
-graph=[]
-visited=[[0]*m for _ in range(n)]
-for i in range(n):
-  graph.append(list(map(int,input().split())))
-visited[x][y] = 1
+def compress(word):
+    count=1
+    cur_word=word[0]
+    ans=[]
+    for a,b in zip(word,word[1:]+['']):
+      #[''] 를 추가함으로써 짝이 안맞더라도 끝까지 탐방함.
+        if a == b:
+          count+=1
+        else:
+           ans.append([count,cur_word])
+           cur_word=b
+           count=1
+           print(cur_word)
+        print(ans)
+    return sum(len(word)+(len(str(count)) if count>1 else 0) for count,word in ans)
+        
+def solution(s):
+    min=len(s)
+    word=[]
+    for unit in range(1,(len(s)//2)+1): #반복 단위
+        for i in range(0,len(s),unit):
+            word.append(s[i:i+unit])
+        ans=compress(word)
+        if ans<min: min=ans
+        word.clear()   
+    return min
 
-dx=[-1,0,1,0] 
-dy=[0,1,0,-1]
-#북 동 남 서
-def turn_left():
-  global direction
-  direction -=1
-  if direction == -1:
-    direction = 3
-#멍청이 ,,, 다시 구현할 필요 없이 -1 하면 되는거를... 멍청이..
-count = 1 #이미 처음 시작한 좌표는 방문한 것이기 때문
-turn_time = 0
-while True:
-  turn_left()
-  nx=x+dx[direction]
-  ny=y+dy[direction]
-  if visited[nx][ny] == 0 and graph[nx][ny] == 0:
-    x=nx
-    y=ny
-    visited[nx][ny] = 1
-    count += 1
-    turn_time = 0
-    #turn_time이라는 것은 회전 수를 표시하는 것인데, 이동하게 되면 다시 새로운 이동을 해야하기때문에 turn_time을 0으로 초기화 해주어야한다.
-    continue
-  else:
-    turn_time += 1
-
-  if turn_time == 4:
-    nx = x-dx[direction]
-    ny = y-dy[direction]
-    if graph[nx][ny] == 1:
-      break
-    visited[nx][ny] = 1
-    x=nx
-    y=ny
-    turn_time = 0
-print(count)
-  
-    
-
-
-
+print(solution("aabbaccc"))
