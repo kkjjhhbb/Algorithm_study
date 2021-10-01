dd4d66d16e595e7a9ec2548f3f484beb394e3c72
JiHyun Kim <37644129+kkjjhhbb@users.noreply.github.com>
2021-10-01 01:47:24 +0000

    Array_Partition 1 - pythonic way

--- leetcode/Array_partition 1.py
+++ leetcode/Array_partition 1.py
@@ -6,3 +6,8 @@ class Solution:
         for i in range(0,len(nums),2):
             ans+=nums[i]  
         return ans
+
+#파이써닉한 방법
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+      return sum(sorted(nums)[::2])
