d0abbba8c9de8a99611645cf4f205342356ccc56
JiHyun Kim <37644129+kkjjhhbb@users.noreply.github.com>
2021-10-01 01:39:56 +0000

    Array Partiton 1

---
+++ leetcode/Array_partition 1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        ans=0
+        pair=[]
+        nums.sort()
+        for i in range(0,len(nums),2):
+            ans+=nums[i]  
+        return ans
