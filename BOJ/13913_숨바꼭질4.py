95762b90c2a5171d1049c2b5683d8ee0168f9fad
jihyun12 <kkjjhhbb@naver.com>
2022-02-01 15:04:39 +0900

    [Baekjoon]13913 숨바꼭질4

---
+++ BOJ/13913_숨바꼭질4.py
@@ -0,0 +1,30 @@
+from collections import deque,defaultdict
+s,e=map(int,input().split())
+dist=[0]*(10**5+1)
+path=defaultdict(list)
+
+def bfs():
+    global path
+    q = deque()
+    q.append(s)
+    path[s]=[]
+    while q:
+        x = q.popleft()
+        if x == e:
+            return
+        for nx in [x+1,x-1,x*2]:
+            if 0<=nx<= 10**5 and not dist[nx]:
+                q.append(nx)
+                dist[nx]=dist[x]+1
+                path[nx].append(x)
+
+bfs()
+now=e
+way=[]
+while now!=s:
+    way.append(now)
+    now = path[now][0]
+way.append(s)
+
+print(dist[e])
+print(*reversed(way),end=' ')
