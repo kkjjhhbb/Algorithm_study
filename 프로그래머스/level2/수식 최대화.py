f3476ef1720433359744fa8b61f17569886ab805
jihyun12 <kkjjhhbb@naver.com>
2022-03-16 16:56:31 +0900

    [Programmers]수식 최대화

---
+++ 프로그래머스/level2/수식 최대화.py
@@ -0,0 +1,22 @@
+import re
+from itertools import permutations
+
+def solution(expression):
+    #1
+    op = [x for x in ['*','+','-'] if x in expression]
+    op = [list(y) for y in permutations(op)]
+    ex = re.split(r'(\D)',expression) #숫자가 아닌 것으로 split
+
+    #2
+    a = []
+    for x in op:
+        _ex = ex[:]
+        for y in x:
+            while y in _ex: #while을 이렇게 사용할 수 있다니..!
+                tmp = _ex.index(y)
+                _ex[tmp-1] = str(eval(_ex[tmp-1]+_ex[tmp]+_ex[tmp+1]))
+                _ex = _ex[:tmp]+_ex[tmp+2:]
+        a.append(_ex[-1])
+
+    #3
+    return max(abs(int(x)) for x in a)
