5a93d2e4b543f12138825ddb2fb6e2bf4a3fb949
KJH <dev.jihyun@gmail.com>
2022-04-05 22:09:48 +0900

    [Baekjoon]두 동전

--- BOJ/16197_두 동전.py
+++ BOJ/16197_두 동전.py
@@ -1,81 +1,43 @@
-from collections import deque
-n,m=map(int,input().split())
-arr=[list(input()) for _ in range(n)]
-mo=[]
-move_list=[]
-answer=[]
-c=[False]*4 #방문했는지
-a=[0]*10 #숫자
-locate=[]
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+def go(step, x1, y1, x2, y2):
+    if step == 11:
+        return -1
+    fall1 = False
+    fall2 = False
+    if x1 < 0 or x1 >= n or y1 < 0 or y1 >= m: #동전1이 떨어졌을 때
+        fall1 = True
+    if x2 < 0 or x2 >= n or y2 < 0 or y2 >= m: #동전2가 떨어졌을 때
+        fall2 = True
+    if fall1 and fall2: #동전이 둘 다 떨어진 경우
+        return -1
+    if fall1 or fall2: #하나만 떨어진 경우
+        return step
+    ans = -1
+    for k in range(4):
+        nx1,ny1 = x1+dx[k],y1+dy[k]
+        nx2,ny2 = x2+dx[k],y2+dy[k]
+        #움직인 위치가 벽일 경우에 이동 전 위치로 바꿔줌
+        if 0 <= nx1 < n and 0 <= ny1 < m and a[nx1][ny1] == '#':
+            nx1,ny1 = x1,y1
+        if 0 <= nx2 < n and 0 <= ny2 < m and a[nx2][ny2] == '#':
+            nx2,ny2 = x2,y2
+        temp = go(step+1,nx1,ny1,nx2,ny2)
+        if temp == -1:
+            continue #정답이랑 비교를 하지 않기위해
+        if ans == -1 or ans > temp:
+            ans = temp
+    return ans
 
+n,m = map(int,input().split())
+x1=y1=x2=y2=-1
+a = [list(input()) for _ in range(n)]
 for i in range(n):
     for j in range(m):
-        if arr[i][j] == 'o':
-            locate.append((i, j))
-
-def find(g):
-    loc=[]
-    for i in range(n):
-        for j in range(m):
-            if g[i][j] == 'o':
-                loc.append((i,j))
-    return loc
-
-def move(arr,locate,mov):
-    global answer
-    graph=[item[:] for item in arr]
-
-    #남 북 서 동
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, -1, 1]
-    cnt=0
-    print(mov)
-
-    for m in mov:
-        if len(find(graph)) == 1:
-            print(*graph,sep='\n')
-            print()
-            print(cnt,'m',m)
-            answer.append(cnt)
-            cnt=0
-            return True
-
-        for l in locate:
-            x,y=l
-            nx=x+dx[m]
-            ny=y+dy[m]
-
-            if 0<=nx<n and 0<=ny<m:
-                if graph[nx][ny] == '.':
-                    graph[nx][ny],graph[x][y]=graph[x][y],graph[nx][ny] #동전을 빈칸으로 옮김
-                    x,y=nx,ny #동전의 위치 변경
-                    print("moving")
-                    print(*graph,sep='\n')
-                    print()
-                elif graph[nx][ny] == '#': #둘이 한꺼번에 움직여야함 벽이면 움직이면 안돼
-                    return False
-
-            else:
-                graph[x][y]='.'
-                x,y=-1,-1
-        cnt+=1
-    # print(*graph, sep='\n')
-    # print()
-    return False
-
-def go(index,start,n):
-    if index == 10:
-        move(arr,locate,a)#움직인다
-        return
-
-    for i in range(start,4):
-        #c[i]=True #사용 방법 검사
-        a[index]=i
-        go(index+1,i,n)
-        c[i]=False
-
-go(0,0,n)
-
-
-print(answer)
-# print(min(answer))
+        if a[i][j] == 'o':
+            if x1 == -1: #첫 동전
+                x1,y1 = i,j
+            else: #두번 째 동전
+                x2,y2 = i,j
+            a[i][j] = '.'
+print(go(0,x1,y1,x2,y2))
