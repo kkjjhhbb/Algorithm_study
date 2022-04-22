4155eaaf0a38e48d248ac1c4fa84a471d5fee96f
KJH <dev.jihyun@gmail.com>
2022-04-22 22:43:06 +0900

    [Baekjoon]감시

--- BOJ/15683_감시.py
+++ BOJ/15683_감시.py
@@ -1,10 +1,11 @@
+from collections import deque
 n,m=map(int(input().split()))
 arr=[list(map(int,input().split)) for _ in range(n)]
 min=0
 dx=[-1,0,1,0]
 dy=[0,1,0,-1]
-
-move={'1': }
+q=deque()
+moves={'1':[[0],[1],[2],[3]],'2':[[1,3],[0,2]],'3':[[0,1],[1,2],[2,3],[0,3]],'4':[[0,1,3],[0,1,2],[1,2,3],[0,2,3]],'5':[[0,1,2,3]] }
 
 def sagak():
     cnt=0
@@ -15,7 +16,9 @@ def sagak():
     return cnt
 
 def bfs(x,y):
-
+    for move in moves[str(arr[x][j])].values():
+        for m in move:
+            x,y=x+dx[]
 
 
 for i in range(n):
