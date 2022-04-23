7a47ee6fa2ade27c1239a0cd7c5f7fc12be25088
jihyun12 <kkjjhhbb@naver.com>
2022-04-23 21:09:50 +0900

    [Baekjoon]감시

--- BOJ/15683_감시.py
+++ BOJ/15683_감시.py
@@ -1,27 +1,58 @@
 from collections import deque
-n,m=map(int(input().split()))
-arr=[list(map(int,input().split)) for _ in range(n)]
-min=0
+n,m=map(int,input().split())
+arr=[]
+cctv_cnt=0
+c_list=[]
+ans=999999
+for i in range(n):
+    a=list(map(int,input().split()))
+    arr.append(a)
+    for j in range(m):
+        if 1<=a[j]<=5:
+            cctv_cnt +=1
+            c_list.append([i,j,arr[i][j]])
 dx=[-1,0,1,0]
 dy=[0,1,0,-1]
 q=deque()
-moves={'1':[[0],[1],[2],[3]],'2':[[1,3],[0,2]],'3':[[0,1],[1,2],[2,3],[0,3]],'4':[[0,1,3],[0,1,2],[1,2,3],[0,2,3]],'5':[[0,1,2,3]] }
+moves=[[],[[0],[1],[2],[3]],[[1,3],[0,2]],[[0,1],[1,2],[2,3],[0,3]],[[0,1,3],[0,1,2],[1,2,3],[0,2,3]],[[0,1,2,3]]]
 
-def sagak():
-    cnt=0
+# moves={'1':[[0],[1],[2],[3]],'2':[[1,3],[0,2]],'3':[[0,1],[1,2],[2,3],[0,3]],'4':[[0,1,3],[0,1,2],[1,2,3],[0,2,3]],'5':[[0,1,2,3]] }
+def bfs(x,y,direction,tmp):
+    print(direction)
+    for d in direction:
+        nx=x
+        ny=y
+        while True:
+            nx, ny = nx + dx[d], ny + dy[d]
+            if 0<=nx<n and 0<=ny<m and tmp[nx][ny]!=6:
+                if tmp[nx][ny] == 0:
+                    tmp[nx][ny]='#'
+                else:
+                    break
+
+
+
+def copy(arr):
+    tmp=[]
     for i in range(n):
-        for j in range(m):
-            if arr[i][j] == 0:
-                cnt+=1
-    return cnt
+        tmp.append(arr[i])
+    return tmp
 
-def bfs(x,y):
-    for move in moves[str(arr[x][j])].values():
-        for m in move:
-            x,y=x+dx[]
+def dfs(arr,cnt):
+    global ans
+    tmp=copy(arr)
+    if cnt == cctv_cnt:
+        num=0
+        for i in range(n):
+            num+=tmp[i].count(0)
+        print(num)
+        ans=min(ans,num)
+        return
 
-
-for i in range(n):
-    for j in range(m):
-        if 1<=arr[i][j]<=5:
-            bfs(i,j)
+    x,y,direction=c_list[cnt]
+    for m in moves[direction]:
+        bfs(x,y,m,tmp)
+        dfs(tmp,cnt+1)
+        tmp=copy(arr)
+dfs(arr,0)
+print(ans)
