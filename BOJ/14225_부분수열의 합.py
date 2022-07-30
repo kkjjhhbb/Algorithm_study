62b37f237aed81846b654126ac95bc288ff495dd
김지현 <jhkim04@miridi.com>
2022-07-30 15:38:37 +0900

    Revert "[Baekjoon]부분수열의 합 새로운 방법"
    This reverts commit 86ddfec3a64b524ba092015585621c29026838ea.

--- BOJ/14225_부분수열의 합.py
+++ BOJ/14225_부분수열의 합.py
@@ -1,36 +1,20 @@
 n=int(input())
 arr=list(map(int,input().split()))
-num_list=[False for _ in range(1,sum(arr)+3)]
+ans=[]
 arr.sort()
+last=sum(arr)
+num_list=[False for _ in range(1,last+2)]
 num_list[0]=True
 
-def go(arr,index,sum):
-    if sum and not num_list[sum]:
-        num_list[sum]=True
+def go(ans,start):
+    if ans:
+        num_list[sum(ans)] = True
 
-    if index == len(arr):
-        return
+    for i in range(start,n):
+        ans.append(arr[i])
+        go(ans,i+1)
+        ans.pop()
 
-    go(arr,index+1,sum+arr[index])
-    go(arr,index+1,sum)
-    return
-go(arr,0,0)
-print(num_list.index(False))
-#
-# arr.sort()
-# last=sum(arr)
-# num_list=[False for _ in range(1,last+2)]
-# num_list[0]=True
-#
-# def go(ans,start):
-#     if ans:
-#         num_list[sum(ans)] = True
-#
-#     for i in range(start,n):
-#         ans.append(arr[i])
-#         go(ans,i+1)
-#         ans.pop()
-#
-# go(ans,0)
-#
-# print(last+1) if False not in num_list else print(num_list.index(False))
+go(ans,0)
+
+print(last+1) if False not in num_list else print(num_list.index(False))
