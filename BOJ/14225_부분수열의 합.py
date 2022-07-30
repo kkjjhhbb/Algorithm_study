8d495e1c37e1674d3ed204417c2b1ebd903a55f4
김지현 <jhkim04@miridi.com>
2022-07-30 15:46:51 +0900

    [Baekjoon]부분수열의 합 새로운 방법

--- BOJ/14225_부분수열의 합.py
+++ BOJ/14225_부분수열의 합.py
@@ -18,3 +18,22 @@ def go(ans,start):
 go(ans,0)
 
 print(last+1) if False not in num_list else print(num_list.index(False))
+
+n=int(input())
+arr=list(map(int,input().split()))
+num_list=[False for _ in range(1,sum(arr)+3)]
+arr.sort()
+num_list[0]=True
+
+def go(arr,index,sum):
+    if sum and not num_list[sum]:
+        num_list[sum]=True
+
+    if index == len(arr):
+        return
+
+    go(arr,index+1,sum+arr[index])
+    go(arr,index+1,sum)
+    return
+go(arr,0,0)
+print(num_list.index(False))
