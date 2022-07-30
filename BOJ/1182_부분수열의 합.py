00cf36a1862b36844191052ebbefe77188dc0a59
김지현 <kkjjhhbb@naver.com>
2022-07-30 16:31:13 +0900

    [Baekjoon]부분수열의 합 새로운 방법

--- BOJ/1182_부분수열의 합.py
+++ BOJ/1182_부분수열의 합.py
@@ -15,3 +15,18 @@ def go(ans,start):
 go(ans,0)
 print(answer)
 
+# o x 로 푸는 문제
+def go(index,sum):
+    global ans
+    if index == n:
+        if sum == s:
+            ans += 1
+        return
+
+    go(index+1,sum+arr[index])
+    go(index+1,sum)
+
+go(0,0)
+if s == 0: #모두 선택하지 않는 경우 처리 ( 모든 것이 포함되지 않았을 때 ) -> 아무것도 선택하지 않아서 sum이 0이 될 수도 있기 때문에
+    ans -= 1
+print(ans)
