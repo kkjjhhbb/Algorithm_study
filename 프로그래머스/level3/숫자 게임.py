f0d1c99d14e84929ee49bd6cfd3f5ddccc843eb7
kkjjhhbb <kkjjhhbb@naver.com>
2022-09-16 11:29:40 +0900

    [Programmers]숫자 게임

--- 프로그래머스/level3/숫자 게임.py
+++ 프로그래머스/level3/숫자 게임.py
@@ -1,27 +1,11 @@
-from collections import deque
-#시간초과
-#heapq 모듈로 풀어보기
 def solution(A, B):
-    A.sort()
-    B.sort()
-    bq = deque(B)
-
-    if A[0] > B[-1]:
-        return 0
-    if A[-1] < B[0]:
-        return len(B)
-
-    i = 0
     answer = 0
-    for i in range(len(A)):
-        while True:
-            if A[i] > max(list(bq)):
-                break
-            if A[i] < bq[0]:
-                bq.popleft()
-                answer += 1
-                break
-            bq.append(bq.popleft())
+    A.sort(reverse = True)
+    B.sort(reverse = True)
+    for a in A:
+        if a >= B[0]:
+            continue
+        else:
+            answer += 1
+            del B[0]
     return answer
-# print(solution([5,1,3,7],[2,2,6,8]))
-print(solution([1,1,1,1,1,1,2,2,2,3,3,4,5],[1,1,1,1,1,1,2,2,2,3,3,4,5]))
