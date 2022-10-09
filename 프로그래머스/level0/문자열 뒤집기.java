99ec269c212ac26c37887d6b53b3efc823dea795
jihyun12 <kkjjhhbb@naver.com>
2022-10-09 18:11:31 +0900

    [java]문자열 뒤집기

---
+++ 프로그래머스/level0/문자열 뒤집기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuffer sb = new StringBuffer(my_string);
+        return sb.reverse().toString();
+    }
+}
+
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder(my_string);
+        return sb.reverse().toString();
+    }
+}
